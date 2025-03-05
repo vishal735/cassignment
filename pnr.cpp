9108fe7f65bc711ba2e15e91284d77c51b1c53e2
vishal <vc009088gmail.com>
2025-03-05 15:36:51 +0530

    c language

--- pnr.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-       float p,n,r,i;
-       printf("\n enter the principle, no of year,rate of intrest:");
-       scanf("%f %f %f",&p,&n,&r);
-       i=(p*n*r)/100;
-       printf("\n simple interst=%f",i);  
-}
